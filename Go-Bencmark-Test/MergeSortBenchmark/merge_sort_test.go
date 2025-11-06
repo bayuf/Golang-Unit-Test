979f2958815d760df783d75941e7258ebfebc36f
Bayu Firmansyah <bayufirmansyah203@gmail.com>
2025-11-06 09:11:14 +0700

    add BenchmarkTable

--- Go-Bencmark-Test/MergeSortBenchmark/merge_sort_test.go
+++ Go-Bencmark-Test/MergeSortBenchmark/merge_sort_test.go
@@ -2,6 +2,34 @@ package mergesort
 
 import "testing"
 
+func BenchmarkTableMergeSort(b *testing.B) {
+	tests := []struct {
+		funcName string
+		datas    []int
+	}{
+		{
+			funcName: "Merge1",
+			datas:    []int{10, 3, 2, 6, 1, 6},
+		},
+		{
+			funcName: "Merge2",
+			datas:    []int{10, 3, 2, 6, 1, 6, 4, 2, 7, 8},
+		},
+		{
+			funcName: "Merge3",
+			datas:    []int{10, 3, 2, 6, 3, 5, 1, 8, 9, 6, 1, 6},
+		},
+	}
+
+	for _, test := range tests {
+		b.Run(test.funcName, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				StartSort(test.datas)
+			}
+		})
+	}
+}
+
 func BenchmarkMergeSortSub(b *testing.B) {
 	// Subtest
 	b.Run("Merge1", func(b *testing.B) {
